ff11d1eebbe1e5bb4c93a7742be5e6a1e6db15e5
Tomasgb <tomasgomezbelcastro@gmail.com>
2021-03-23 21:20:15 -0300

    full functioning app with google login and backend database record on firebase. plus certain functionality on mobile devices

--- script.js
+++ script.js
@@ -9,127 +9,204 @@ const submitBook  = document.getElementById('submit');
 const iconclose   = document.getElementById('iconclose');
 const container   = document.getElementById('container');
 
+const auth          = firebase.auth();
+const db            = firebase.firestore();
+const whenSignedIn  = document.getElementById('loggedin');
+const whenSignedOut = document.getElementById('loggedout');
 
+const signinBttn   = document.getElementById('loginbutton');
+const logoutBttn  = document.getElementById('logoutbutton');
+const userDetails = document.getElementById('userdetails');
 
-newBook.addEventListener('click', function(){  //button that opens input form, and hiddes container elements
+const provider = new firebase.auth.GoogleAuthProvider();
 
-    let bookshow = document.querySelectorAll('.bookInfo');
-    bookshow.forEach(element => element.hidden = true)
+let booksRef;
+let unsubscribe;
 
-    form.style.display = 'inline';
-    newBook.hidden= true;
+signinBttn.addEventListener('click', function(){ 
+
+    
+
+    return auth.signInWithPopup(provider);
 })
 
-iconclose.addEventListener('click', function(){   //closing form icon. shows hidden elements of container
+logoutBttn.addEventListener('click', function(){
 
-    form.style.display = 'none';
-    newBook.hidden = false;
+    
+    return auth.signOut();
 })
 
+auth.onAuthStateChanged(user => {
+    if (user) {
+        userDetails.innerHTML = `Welcome to your library ${user.displayName} !`
+        let profilepic = document.createElement('img');
+        profilepic.classList.add('profilepic')
+        profilepic.setAttribute('src', user.photoURL);
+        userDetails.appendChild(profilepic); 
+
+        whenSignedOut.style.display = 'none';
+        whenSignedIn.style.display = 'block';
+        newBook.removeAttribute('disabled');
+        container.style.pointerEvents = 'auto';
+
+      booksRef = db.collection('books');
+        forDisplay();
 
 
-submitBook.addEventListener('click',addBook);   //submits the book for showing and display on container
 
 
+    newBook.addEventListener('click', function(){  //button that opens input form, and hiddes container elements
 
+        let bookshow = document.querySelectorAll('.bookInfo');
+        bookshow.forEach(element => element.hidden = true)
 
-function addBook(){                           //if title and author are there adds book to the library array and calls display func.
+        form.style.display = 'inline';
+        newBook.hidden= true;
 
-    if(titleInput.value == 0 || authorInput.value == 0){
-
-        alert('Please, introduce at least Title and Author of the book')
         
-    }else{
-            
-    let newObject = nuevo();
-
-
-    form.style.display = 'none';
-    newBook.hidden = false;
-    alert('YOUR BOOK WAS ADDED TO YOUR LIBRARY !')
-
-
-    myLibrary.push(newObject);
-
-     return forDisplay();
-    }
-    
-
-    
-}
-
-function nuevo(){        //object creator for books.
-    
-        return {
-             
-            title: titleInput.value,
-            author: authorInput.value,
-            pages: pagesInput.value,
-            read: readInput.value
-        }
-        
-};
-
-function forDisplay(){     //on every run, deletes displayed elements and generate the newones.
-
-    let elements = document.querySelectorAll('.onDisplay');
-    elements.forEach(element => element.remove());
-
-
-
-    myLibrary.forEach(function(element){
-
-        let book = document.createElement('div');
-        book.classList.add('onDisplay');
-        let bookinfo = document.createElement('div');
-        bookinfo.classList.add('bookInfo');
-
-        let finished = document.createElement('div');
-        if(element.read == 'YES'){
-            finished.classList.add('finishedyes');
-        }else{ finished.classList.add('finishedno')};
-        
-        
-
-        bookinfo.innerHTML =`<p class= "ondisplaytext" ><strong>Title: </strong>${element.title}<br><strong> Author: </strong>${element.author}<br> <strong>Pages: </strong> ${element.pages}<br></p>`;
-
-        bookinfo.appendChild(finished);
-
-        book.setAttribute('data-index', `${myLibrary.indexOf(element)}`);
-        book.appendChild(bookinfo);
-
-        bookinfo.setAttribute('data-index', book.getAttribute('data-index'));
-        
-        book.addEventListener('dblclick', function(){
-            
-            let index = this.getAttribute('data-index');
-            myLibrary.splice(index, 1);
-            
-            this.remove();
-            return forDisplay();
-
-        })
-
-        finished.addEventListener('click', function(){
-
-            finished.classList.toggle('finishedyes');
-            finished.classList.toggle('finishedno');
-            let index = this.parentNode.getAttribute('data-index');
-
-            let target = myLibrary[index];
-
-            if(target.read == 'YES'){
-
-               return target.read = 'NO';
-            }else{  return target.read = 'YES'};
-
-        })
-        
-        titleInput.value = "";
-        author.value = "";
-        pages.value = "";
-        readInput.value = false;
-        return container.appendChild(book);
     })
+
+    iconclose.addEventListener('click', function(){   //closing form icon. shows hidden elements of container
+
+    form.style.display = 'none';
+    newBook.hidden = false;
+    let bookshow = document.querySelectorAll('.bookInfo');
+    bookshow.forEach(element => element.hidden = false);
+    })
+
+
+
+    submitBook.addEventListener('click',addBook);   //submits the book for showing and display on container
+
+
+
+
+    function addBook(){                           //if title and author are there adds book to the library array and calls display func.
+
+        if(titleInput.value == 0 || authorInput.value == 0){
+
+           alert('Please, introduce at least Title and Author of the book')
+        
+        }else{
+            
+        let newObject = nuevo();
+
+
+        form.style.display = 'none';
+        newBook.hidden = false;
+        alert('YOUR BOOK WAS ADDED TO YOUR LIBRARY !')
+
+
+        myLibrary.push(newObject);
+
+        booksRef.add(newObject)
+        console.log(booksRef)
+
+        return forDisplay();
+        }
+    
+
+    
     }
+
+    function nuevo(){        //object creator for books.
+
+        const { serverTimestamp} = firebase.firestore.FieldValue;
+    
+            return {
+                uid: user.uid,
+                title: titleInput.value,
+                author: authorInput.value,
+                pages: pagesInput.value,
+                read: readInput.value,
+                createdAt: serverTimestamp()
+            }
+        
+    };
+
+        function forDisplay(){     //on every run, deletes displayed elements and generate the newones.
+
+       
+                
+        let elements = document.querySelectorAll('.onDisplay');
+        console.log(elements);
+        elements.forEach(element => element.remove());
+
+
+       window.unsubscribe = booksRef.where('uid', '==', user.uid)
+        .get()
+        .then((querySnapshot) => {
+
+            querySnapshot.forEach((obj) => {
+
+                let onDisplay = document.createElement('div');
+                onDisplay.classList.add('onDisplay');
+                onDisplay.setAttribute('data-id', obj.id);
+
+                onDisplay.addEventListener('dblclick', function(){
+
+                    this.remove();
+
+                    return booksRef.doc(obj.id).delete();
+                })
+
+                let bookinfo = document.createElement('div');
+                bookinfo.classList.add('bookInfo');
+                let finished = document.createElement('div');
+                    if(obj.data().read == 'YES'){
+                        finished.classList.add('finishedyes');
+                    }else{ finished.classList.add('finishedno')}
+
+                    finished.setAttribute('data-id', obj.id)
+
+                    finished.addEventListener('click', function(){
+
+                        finished.classList.toggle('finishedyes');
+                        finished.classList.toggle('finishedno');
+                        if(obj.data().read == 'NO'){
+
+                           return booksRef.doc(obj.id).update({
+
+                            read: 'YES'
+
+                           });
+                        } else { return booksRef.doc(obj.id).update({
+
+                            read: 'NO'
+                        })}
+                    })
+
+                    bookinfo.innerHTML =`<p class= "ondisplaytext" ><strong>Title: </strong>${obj.data().title}<br>
+                    <strong> Author: </strong>${obj.data().author}<br> <strong>Pages: </strong> ${obj.data().pages}<br></p>`
+
+                bookinfo.appendChild(finished);
+                onDisplay.appendChild(bookinfo);
+
+                titleInput.value = "";
+                author.value = "";
+                pages.value = "";
+                readInput.value = false;
+
+                return container.appendChild(onDisplay);
+
+                        })
+                        
+        })
+
+       
+}
+      
+} else { 
+
+        unsubscribe && unsubscribe();
+    
+        whenSignedOut.style.display = 'block';
+        whenSignedIn.style.display =  'none';
+        newBook.setAttribute('disabled', "")
+        container.style.pointerEvents = 'none';
+    
+        let hiddenbooks =document.querySelectorAll('.onDisplay');
+        hiddenbooks.forEach(book => book.style.visibility = 'hidden');
+    }
+});
 
